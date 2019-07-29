e930a373e9f5016f49fb0571a74005a5f813efbe
Jasper Stephenson <jasperstephenson@gmail.com>
2019-07-29 17:47:52 +0200

    Added Procfile

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const options = {
   prefix: '!',
   contactPoint: `jasp`,
   bannedWords: ['nword', 'fword', 'shortfword'],
-  calloutMessage: `Hey! That kind of language is a no go. We take hate speech seriously around here.
+  calloutMessage: `Hey! That kind of language is unacceptable. We take hate speech seriously around here.
 
 The channel moderators have been notified, and your user id has been logged across all servers using this bot. Think twice before using hate speech next time!
 
